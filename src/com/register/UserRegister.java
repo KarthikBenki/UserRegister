5466aacd89697ffc7c3a2f39196c4a05620f9cb2
karthikbenki <karthikmc007@gmail.com>
2022-03-04 07:11:42 +0530

    Added: LastName validation

--- src/com/register/UserRegister.java
+++ src/com/register/UserRegister.java
@@ -6,7 +6,12 @@ import java.util.regex.Pattern;
 public class UserRegister {
     static Scanner sc = new Scanner(System.in);
 
-
+    /*
+       @purpose: Ability to add first name
+       @param: No parameters
+       @function : FirstName Validation
+       @return: No return value.
+      */
     public static void firstName() {
         System.out.println("Enter First Name");
         String fN = sc.nextLine();
@@ -15,7 +20,24 @@ public class UserRegister {
         else
             System.out.println("Invalid First Name");
     }
+
+    /*
+       @purpose: Ability to add last name
+       @param: No parameters
+       @function : last Validation
+       @return: No return value.
+      */
+    public static void lastName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nEnter Last Name");
+        String lN = sc.nextLine();
+        if (Pattern.matches("^[A-Z]{1}[a-z]{2,}$", lN) == true)
+            System.out.println("Valid Last Name");
+        else
+            System.out.println("Invalid Last Name");
+    }
     public static void main(String[] args) {
         firstName();
+        lastName();
     }
 }
